7050ef87123a80de8a376208684aedbb275e600a
Aditya Sharma <adityasharmavats2811@gmail.com>
2025-01-19 14:12:47 +0530

    slug issue

--- client/src/components/PostListItem.jsx
+++ client/src/components/PostListItem.jsx
@@ -14,7 +14,7 @@ const PostListItem = ({post}) => {
       </div>}
 
       <div className="flex flex-col gap-4 xl:w-2/3">
-        <Link to="/test" className="text-4xl font-semibold">
+        <Link to={`/${post.slug}`} className="text-4xl font-semibold">
           {post.title}
         </Link>
         <div className="flex items-center gap-2 text-gray-400 text-sm">
