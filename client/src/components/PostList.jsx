3cdc1ed2219fd2171d26b7cfb5260a7da1b3529b
Aditya Sharma <adityasharmavats2811@gmail.com>
2025-01-19 14:10:50 +0530

    infinite-scroll

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -1,51 +1,56 @@
 import React from 'react'
 import PostListItem from './PostListItem'
-import { useQuery } from '@tanstack/react-query'
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query'
 import axios from "axios"
+import InfiniteScroll from "react-infinite-scroll-component";
 import { use } from 'react'
 
-const fetchPosts =async()=>{
-  const res=await axios.get(`${import.meta.env.VITE_API_URL}/posts`);
+const fetchPosts =async(pageParam)=>{
+  const res=await axios.get(`${import.meta.env.VITE_API_URL}/posts`, {
+    params: { page: pageParam, limit: 2 },
+  });
   return res.data;
 }
 
 
+
 const PostList = () => {
-  const {isPending,error,data}=useQuery({
-    queryKey:["reportData"],
-    queryFn:()=>fetchPosts(),
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
+    queryKey: ["posts"],
+    queryFn: ({ pageParam = 1 }) => fetchPosts(pageParam),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, pages) =>
+      lastPage.hasMore ? pages.length + 1 : undefined,
   });
-
-  if(isPending) return "Loading...";
-  if(error) return "An error has occured: "+ error.message
+  if (isFetching) return "Loading...";
+  if (error) return "Something went wrong!";
   
-  console.log(data)
-  return (
-    <div className='flex flex-col gap-12 mb-8'>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
-      <PostListItem/>
+  const allPosts = data?.pages?.flatMap((page) => page.posts) || [];
 
-    </div>
+  return (
+    <InfiniteScroll
+      dataLength={allPosts.length}
+      next={fetchNextPage}
+      hasMore={!!hasNextPage}
+      loader={<h4>Loading more posts...</h4>}
+      endMessage={
+        <p>
+          <b>All posts loaded!</b>
+        </p>
+      }
+    >
+      {allPosts.map((post) => (
+        <PostListItem key={post._id} post={post} />
+      ))}
+    </InfiniteScroll>
   )
 }
 
--- client/src/components/PostListItem.jsx
+++ client/src/components/PostListItem.jsx
@@ -1,33 +1,33 @@
 import { Link } from "react-router-dom";
 import Image from "./Image";
-const PostListItem = ({}) => {
+import { format } from "timeago.js";
+
+const PostListItem = ({post}) => {
   return (
     <div className="flex flex-col xl:flex-row gap-8 mb-12">
-      <div className="md:hidden xl:block xl:w-1/3">
+      {post.img && <div className="md:hidden xl:block xl:w-1/3">
         <Image
-          src="postImg.jpeg"
+          src={post.img}
           className="rounded-2xl object-cover"
           w="735"
         />
-      </div>
+      </div>}
 
       <div className="flex flex-col gap-4 xl:w-2/3">
         <Link to="/test" className="text-4xl font-semibold">
-          Lorem ipsum dolor sit amet consectetur adipisicing.
+          {post.title}
         </Link>
         <div className="flex items-center gap-2 text-gray-400 text-sm">
           <span>Written by</span>
-          <Link className="text-blue-800">Adi </Link>
+          <Link className="text-blue-800">{post.user.username} </Link>
           <span>on</span>
-          <Link className="text-blue-800">Web Design</Link>
-          <span>2 days ago</span>
+          <Link className="text-blue-800">{post.category}</Link>
+          <span>{format(post.createdAt)}</span>
         </div>
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum
-          dignissimos odit doloremque aspernatur magnam quia fugiat iste esse
-          libero
+          {post.desc}
         </p>
-        <Link to="/test" className="underline text-blue-800 text-sm">
+        <Link to={`/${post.slug}`} className="underline text-blue-800 text-sm">
           Read More
         </Link>
       </div>
