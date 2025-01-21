236bb68fbc60eaf5b58dd15ac9e245f260134b93
Aditya Sharma <adityasharmavats2811@gmail.com>
2025-01-21 21:31:17 +0530

    minor issue fixed

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -16,8 +16,19 @@ const Search = () => {
       }
     }
   };
+
+  const handleSearchIconClick = () => {
+    const query = document.querySelector("input[type='text']").value.trim();
+    if (query) {
+      if (location.pathname === "/posts") {
+        setSearchParams({ ...Object.fromEntries(searchParams), search: query });
+      } else {
+        navigate(`/posts?search=${query}`);
+      }
+    }
+  };
   return (
-    <div className="bg-gray-100 p-2 rounded-full flex items-center gap-2">
+    <div className="bg-yellow-100 p-2 rounded-full flex items-center gap-2">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
@@ -25,6 +36,8 @@ const Search = () => {
         height="20"
         fill="none"
         stroke="gray"
+        onClick={handleSearchIconClick}
+        className="hover:cursor-pointer"
       >
         <circle cx="10.5" cy="10.5" r="7.5" />
         <line x1="16.5" y1="16.5" x2="22" y2="22" />
@@ -32,7 +45,7 @@ const Search = () => {
       <input
         type="text"
         placeholder="search a post..."
-        className="bg-transparent"
+        className="bg-transparent text-red-950"
         onKeyDown={handleKeyPress}
 
       />
